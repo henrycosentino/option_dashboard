3b3ecae5fbb339970f58c3cfeca3704784e68300
henrycosentino <henry_fc@icloud.com>
2025-03-20 07:57:47 -0700

    Update BlackScholes.py

--- BlackScholes.py
+++ BlackScholes.py
@@ -7,10 +7,10 @@ class BlackScholes:
     def __init__(self, k, s, r, t, iv, b):
         self.k = k # Strike
         self.s = s # Spot
-        self.r = r # Risk free rate (annual)
-        self.t = t # Time (fraction of years)
-        self.iv = iv # Implied volatility
-        self.b = b # Dividend rate (annual)
+        self.r = r # Risk free rate (annual; input in decimal form ie 5.0% --> 0.05)
+        self.t = t # Time (fraction of years; input in decimal form ie 3 months --> 0.25)
+        self.iv = iv # Implied volatility (input in decimal form ie 20.0% --> 0.20)
+        self.b = b # Dividend rate (annual; input in decimal form ie 2.0% --> 0.02)
 
     def _d1(self):
         return (np.log(self.s/self.k) + (self.b + self.iv**2 * 0.5) * self.t) / (np.sqrt(self.t) * self.iv)
@@ -75,7 +75,7 @@ class BlackScholes:
     def charm(self, optionType):
         optionType = optionType.upper()
         term1 = norm.pdf(self._d1()) * ((self.b / (self.iv * np.sqrt(self.t))) - (self._d2() / (2 * self.t)))
-        term2 = (self.b - self.r) * norm.cdf(self._d1())  # Common term for call
+        term2 = (self.b - self.r) * norm.cdf(self._d1())
         if optionType == 'CALL':
             return -np.exp((self.b - self.r) * self.t) * (term1 + term2) / 252
         elif optionType == 'PUT':
